ab1dd21cfd2c864cc8df52ac34d8dd7f30f503cf
mateuszGadecki <mateusz.gadecki98@gmail.com>
2020-08-15 18:34:57 +0200

    Adding a New Item to the User Interface

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ var budgetController = (function() {
         addItem: function(type ,des, val) {
             var newItem, ID;
             // Creating new ID
-            ID = data.allItems[type][data.allItems[type].length - 1].id
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1
+            } else { ID = 0; }
 
             // Creating new item based on 'inc' or 'exp'
             if(type === 'exp') {
@@ -41,7 +43,8 @@ var budgetController = (function() {
             // Return the new element
             return newItem;
         }
-    }
+
+    };
 
 })();
 
@@ -52,7 +55,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue:'.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
 
     return {
@@ -62,6 +67,27 @@ var UIController = (function() {
             description: document.querySelector(DOMstrings.inputDescription).value,
             value: document.querySelector(DOMstrings.inputValue).value
             };
+        },
+
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+            // Create HTML string with placeholder text
+            if(type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if(type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            // Replace the placeholder text
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert the HMTL into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
         },
         getDOMstrings: function() {
             return DOMstrings;
@@ -84,11 +110,16 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
     var ctrlAddItem = function() {
+        var input, newItem;
+
         // 1. Get the field input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
+
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
+        UICtrl.addListItem(newItem, input.type);
 
         // 4. Calculate the budget
 
