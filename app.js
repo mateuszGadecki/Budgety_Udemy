529d1792831cdccab92be36b490cdc911f93bc16
mateuszGadecki <mateusz.gadecki98@gmail.com>
2020-08-28 12:41:09 +0200

    Displaying the current month and year

--- app.js
+++ app.js
@@ -140,7 +140,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
 
     var formatNumber = function(num, type) {
@@ -243,6 +244,15 @@ var UIController = (function() {
             });
         },
 
+        displayMonth: function() {
+            var now, year, month, months;
+            now = new Date();
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -343,6 +353,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     return {
         init: function() {
+            UICtrl.displayMonth();
             UICtrl.displayBudget( {
                 budget: 0,
                 totalInc: 0,
